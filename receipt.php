284993cb8d8568c5b3a527911cb597bf218ccccf
harsh42774 <31183552+harsh42774@users.noreply.github.com>
2018-03-31 18:22:33 +0530

    Major Changes
    -> Major changes to UI and many other parts of code for efficiency

--- receipt.php
+++ receipt.php
@@ -7,15 +7,71 @@ $sql=mysqli_query($con,"SELECT * from data WHERE reg='$reg'");
 echo "<html><head><script type='text/javascript'>
 	window.history.forward();
 	function noBack() { window.history.forward(); }
-	</script></head>
+	</script>
+	<style>.container{
+        border-width: 3px;
+        border-spacing: 5px;
+        border-style: inset;
+        border-color: blue;
+        border-radius: 5px;
+		margin:auto; width:300px; height: 150px; background-color: rgb(204,204,255);} 
+		body{background-image:url('background.jpg');}
+		input[type=submit] {
+		border-radius: 5px;
+		background-color: rgb(200, 200, 255);
+		border-color: blue;
+		color: black;
+		text-align: center;
+		font-size: 15px;
+		padding: 10px;
+		width: 120px;
+		transition: all 0.5s;
+		cursor: pointer;
+		margin: 5px;
+		font:Times New Roman;
+		}
+
+		input[type=submit] span {
+		cursor: pointer;
+		display: inline-block;
+		position: relative;
+		transition: 0.5s;
+		}
+
+		input[type=submit] span:after {
+		content: '\00bb';
+		position: absolute;
+		opacity: 0;
+		top: 0;
+		right: -20px;
+		transition: 0.5s;
+		}
+
+		input[type=submit]:hover span {
+		padding-right: 25px;
+		}
+
+		input[type=submit]:hover span:after {
+		opacity: 1;
+		right: 0;
+		}
+
+	</style>
+	<script type='text/javascript'>
+	window.history.forward();
+	function noBack() { window.history.forward(); }
+	</script>
+	</head>
 	<body onload='noBack();' 
 	onpageshow='if (event.persisted) noBack();' onunload='s'>
-	<h1>Receipt</h1>";
+	<br><br><br><br>
+	<div class='container'>
+	<center><h3><u>Receipt</u></h3>";
 
 while($row=mysqli_fetch_array($sql))
 {
 	$room_no=$row['room_no'];
-	echo "<table><tr><td>Hostel Fees</td><td>".$row['room_fees']."</td></tr><tr><td>Mess Fees</td><td>".$row['mess_fees']."</td></tr></table></body></html>";
+	echo "<table cellspacing='10'><tr><td>Hostel fees(INR):</td><td>".$row['room_fees']."</td></tr><tr><td>Mess fees(INR):</td><td>".$row['mess_fees']."</td></tr></table></center></div></body></html>";
 }
 
 $sql=mysqli_query($con,"SELECT * FROM extras");
--- room.php
+++ room.php
@@ -10,7 +10,70 @@ while($row=mysqli_fetch_array($sql))
     $type=$row['type'];
 }
 
-echo "<html><head><title>Room Selection</title></head><body><p align='center'><h3>Select Room Number</h3><form method='post' action='room1.php'><select name='room_no'>";
+echo "<html>
+    <head>
+        <style>.container{
+        border-width: 3px;
+        border-spacing: 5px;
+        border-style: inset;
+        border-color: blue;
+        border-collapse: collapse;
+        border-radius: 5px;
+        margin:auto; width:300px; height: 100px; background-color: rgb(204,204,255);} body{background-image:url('background.jpg');}
+        input[type=submit] {
+        border-radius: 5px;
+        background-color: rgb(200, 200, 255);
+        border-color: blue;
+        color: black;
+        text-align: center;
+        font-size: 15px;
+        padding: 10px;
+        width: 120px;
+        transition: all 0.5s;
+        cursor: pointer;
+        margin: 5px;
+        font:Times New Roman;
+        }
+
+        input[type=submit] span {
+        cursor: pointer;
+        display: inline-block;
+        position: relative;
+        transition: 0.5s;
+        }
+
+        input[type=submit] span:after {
+        content: '\00bb';
+        position: absolute;
+        opacity: 0;
+        top: 0;
+        right: -20px;
+        transition: 0.5s;
+        }
+
+        input[type=submit]:hover span {
+        padding-right: 25px;
+        }
+
+        input[type=submit]:hover span:after {
+        opacity: 1;
+        right: 0;
+        }
+        
+        input[type=select]{
+            border-radius: 10px;
+        }
+
+        </style>
+        <title>Room Selection</title>
+    </head>
+    <body>
+    <br><br><br><br>
+    <form method='post' action='room1.php'>
+        <div class = 'container'>
+            <center>
+                <h3>Select Room Number</h3>
+                <select name='room_no'>";
 
 $sql=mysqli_query($con,"SELECT * FROM rooms WHERE alloted='0' AND bed='$bed' AND type='$type'");
 while($row=mysqli_fetch_array($sql))
@@ -19,7 +82,7 @@ while($row=mysqli_fetch_array($sql))
     echo "<option value='$room_no'>$room_no</option>";
 }
 
-echo "</select><br>
-    <input type='submit' name='submit' value='Submit'></form></p></body></html>";
+echo "</select>
+    </center></div><br><center><input type='submit' name='submit' value='Submit'></center></form></body></html>";
 
 ?>
