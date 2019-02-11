2a72df103b4816d1bbc10a330048d744aaff11d5
Siddharth Kale <siddharthkale97@gmail.com>
2019-02-11 21:56:09 +0530

    if registraion done redirect only to receipt page

--- check.php
+++ check.php
@@ -1,17 +1,17 @@
 <?php
 require('sql_connect.php');
 $reg=strtoupper($_COOKIE['reg']);
-$sql1=mysqli_query($con,"SELECT * FROM login WHERE reg='$reg'");
-$sql=mysqli_query($con,"SELECT * FROM data WHERE reg='$reg'");
+$sql1=mysqli_query($con,"SELECT * FROM login WHERE reg='$reg'");// for getting alloted field data
+$sql=mysqli_query($con,"SELECT * FROM data WHERE reg='$reg'");// for getting room occupied by students data
 
 while($row1=mysqli_fetch_array($sql1))
 {
-	if($row1['alloted']==0)
+	if($row1['alloted']==0)// not alloted
 	{
 		header('Location: blockselection.htm');
 		exit();
 	}
-	else
+	else// if alloted
 	{
 		while($row=mysqli_fetch_array($sql))
 		{
@@ -28,7 +28,7 @@ while($row1=mysqli_fetch_array($sql1))
 						location.href='mess1.htm';
 						</script>");
 					exit();
-				}	
+				}
 				else if(!empty($row['r3']))
 				{
 					echo("<script>alert('You have already been registered for room with $r1, $r2, $r3 but your mess selection is pending ');
@@ -53,14 +53,26 @@ while($row1=mysqli_fetch_array($sql1))
 			}
 			else
 			{
+				// redirect to reciept (reciept.php)
 				echo("<script language='JavaScript'>
 					window.alert('You have already registered for room and mess')
-					window.location.href='main.html'
+					window.location.href='receipt.php'
 					</script>");
 				exit();
 			}
-		}			
+		}
 	}
 }
+
+// what to add
+/*
+
+1. add to wait if all roomatates have not accepted(for leader who is sending requests)
+2. if stalling some one create a stall page
+3. logout then login(request accept person), stall at main page, please wasot for leader to get a roomate
+4. when all has accepted redrect leader to room selection (oomselection.htm)
+
+*/
+
 
 ?>
